f62c0d7fd5d61d3fa313d44cef8e81942507e9b6
Minisock <goldenkitamar@gmail.com>
2024-07-18 12:06:52 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Amarjeet Kumar on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
